a1c816f2e687c05620bc4917b8b500cc8598070b
ar.gayazov <a.gayazov@bk.ru>
2023-08-28 12:45:37 +0300

    fix

--- installer.py
+++ installer.py
@@ -90,7 +90,7 @@ def clone():
     if not os.path.exists(products[product_name]['Path']):
         subprocess.run(f'git clone {repo}', cwd=path, shell=True)
 
-    #накатываем uatf
+    # накатываем uatf
     repo = products['uatf']['Repo']
     subprocess.run(f'git clone {repo}', cwd=path, shell=True)
 
@@ -101,12 +101,28 @@ def create_venv():
     path_env = os.path.join(__get_config_option('path_tests'), __get_config_option('main_product'), 'venv')
     subprocess.run(f'{python} -m venv --system-site-packages {path_env}', shell=True)
 
+    config = ConfigParser()
+    if os.path.isfile(path_conf):
+        config.read(path_conf, encoding=encoding)
+    if not config.has_section(section):
+        config.add_section(section)
+    config.set(section, 'path_env', path_env)
+    with open(path_conf, 'w+', encoding=encoding) as config_file:
+        config.write(config_file)
+
+
+def install_requirements():
+    """Установить зависимости"""
+
+    path_tests, path_env = __get_config_option('path_tests'), __get_config_option('path_env')
+    subprocess.run(f'{path_env}/{folder}/pip install -r {path_tests}/uatf/requirements.txt', shell=True)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Clone/update/checkout repositories')
     parser.add_argument('-clone', action='store_true', help='Clone repositories')
     parser.add_argument('-create_venv', action='store_true', help='Create venv')
-    # parser.add_argument('-reqs', action='store_true', help='Install requirements')
+    parser.add_argument('-reqs', action='store_true', help='Install requirements')
     # parser.add_argument('-checkout', type=str, help='Checkout branches')
     # parser.add_argument('-pull', action='store_true', help='Pull branches')
     args = parser.parse_args()
@@ -114,8 +130,8 @@ if __name__ == '__main__':
         clone()
     if args.create_venv:
         create_venv()
-    # if args.reqs:
-    #     install_requirements()
+    if args.reqs:
+        install_requirements()
     # if args.checkout:
     #     checkout(args.checkout)
     # if args.pull:
